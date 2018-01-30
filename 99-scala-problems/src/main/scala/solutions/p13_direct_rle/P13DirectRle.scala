a967fc3200dfd5ab119657c2e57fb5fde1afdb08
valio_stoyanov <valentinstoqnov98@gmail.com>
2018-01-30 21:27:26 +0200

    Direct rle implementation

---
+++ 99-scala-problems/src/main/scala/solutions/p13_direct_rle/P13DirectRle.scala
@@ -0,0 +1,21 @@
+package solutions.p13_direct_rle
+
+object P13DirectRle {
+
+  def encodeDirect(list: List[Symbol]): List[(Int, Symbol)] = {
+
+    def encodeRecursive(xs: List[Symbol], res: List[(Int, Symbol)] = Nil): List[(Int, Symbol)] = xs match {
+      case Nil => res
+      case x :: tail => encodeRecursive(tail.dropWhile(x ==), res ++ List((tail.takeWhile(x ==).length + 1, x)))
+    }
+    
+    encodeRecursive(list)
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    println(encodeDirect(list))
+  }
+
+}
